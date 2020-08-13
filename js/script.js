fe61deeaf6495e5b56c7b8b7496c996bb292a87f
Bruno Figueiredo <test@jdjf.com>
2020-08-13 10:58:04 +0200

    Smoother mobile menu close open animation

--- js/script.js
+++ js/script.js
@@ -124,10 +124,10 @@ $(window).scroll(function () {
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
     document.getElementById("mySidenav").style.width = "100vw";
-    $('#main').fadeOut();
+    $('#main').hide(400);
     $('#mySidenav li').fadeIn(400);
     $('#mySidenav li').css('margin-left', '10px');
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.body.style.backgroundColor = "#2c2f33";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
@@ -136,5 +136,4 @@ function closeNav() {
     $('#main').fadeIn();
     $('#mySidenav li').fadeOut(400);
     $('#mySidenav li').css('margin-left', '-300px');
-    document.body.style.backgroundColor = "white";
 }
